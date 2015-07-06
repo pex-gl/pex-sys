4eaa2861de2306b192c1d5268984c3e95f6a9ef4
Marcin Ignac <marcin.ignac@gmail.com>
2015-07-06 13:00:55 +0100

    Init

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-module.exports.Platform = require('./lib/Platform');
-module.exports.Window = require('./lib/Window');
-module.exports.Time = require('./lib/Time');
-module.exports.IO = require('./lib/IO');
-module.exports.Log = require('./lib/Log');
+module.exports = {
+    Platform: require('./lib/Platform'),
+    Window: require('./lib/Window')
+};
