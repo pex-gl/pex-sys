f846dff446f158e5a60fe95d0ffb198c790b8503
Henryk Wollik <olafyoorsen@hotmail.com>
2015-07-15 16:12:37 +0200

    Mouse get{X,Y,PrevX,PrevY} => getPos{X,Y},getPrevPos{X,Y}, added getPos,getPrevPos,getDelta, moved delta calc to move

--- Mouse.js
+++ Mouse.js
@@ -8,35 +8,64 @@ function Mouse() {
     this._y = 0;
     this._prevX = 0;
     this._prevY = 0;
+    this._deltaX = 0;
+    this._deltaY = 0;
     this._isDown = false;
 }
 
 Mouse.prototype = Object.create(EventDispatcher.prototype);
 Mouse.prototype.constructor = Mouse;
 
-Mouse.prototype.getX = function() {
+Mouse.prototype.getPosX = function() {
     return this._x;
-}
+};
 
-Mouse.prototype.getY = function() {
+Mouse.prototype.getPosY = function() {
     return this._y;
-}
+};
 
-Mouse.prototype.getPrevX = function() {
+Mouse.prototype.getPos = function(out){
+    if(out === undefined){
+        return [this._x,this._y];
+    }
+    out[0] = this._x;
+    out[1] = this._y;
+    return out;
+};
+
+Mouse.prototype.getPrevPosX = function() {
     return this._prevX;
-}
+};
 
-Mouse.prototype.getPrevY = function() {
+Mouse.prototype.getPrevPosY = function() {
     return this._prevY;
-}
+};
+
+Mouse.prototype.getPrevPos = function(out){
+    if(out === undefined){
+        return [this._prevX,this._prevY];
+    }
+    out[0] = this._prevX;
+    out[1] = this._prevY;
+    return out;
+};
 
 Mouse.prototype.getDeltaX = function() {
-    return this._x - this._prevX;
-}
+    return this._deltaX;
+};
 
 Mouse.prototype.getDeltaY = function() {
-    return this._y - this._prevY;
-}
+    return this._deltaY;
+};
+
+Mouse.prototype.getDelta = function(out){
+    if(out === undefined){
+        return [this._deltaX,this._deltaY];
+    }
+    out[0] = this._deltaX;
+    out[1] = this._deltaY;
+    return out;
+};
 
 Mouse.prototype.handleMouseDown = function(e) {
     this._isDown = true;
@@ -55,6 +84,8 @@ Mouse.prototype.handleMouseMove = function(e) {
     this._prevY = this._y;
     this._x = e.x;
     this._y = e.y;
+    this._deltaX = this._x - this._prevX;
+    this._deltaY = this._y - this._prevY;
 
     e.mouse = this;
 
