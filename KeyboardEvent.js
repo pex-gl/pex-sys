b9de3a064e671d44da8e5f269e4a4e49675b61ff
Marcin Ignac <marcin.ignac@gmail.com>
2015-07-22 17:15:32 +0200

    KeyboardEvent added virtual key codes VK_*

--- KeyboardEvent.js
+++ KeyboardEvent.js
@@ -1,4 +1,5 @@
 var Event = require('./Event');
+var isBrowser = require('is-browser');
 
 function KeyboardEvent(type, data) {
     Event.call(this, type, data);
@@ -10,5 +11,16 @@ KeyboardEvent.prototype.constructor = KeyboardEvent;
 KeyboardEvent.KEY_DOWN  = 'keydown';
 KeyboardEvent.KEY_PRESS = 'keypress';
 KeyboardEvent.KEY_UP    = 'keyup';
+
+KeyboardEvent.VK_BACKSPACE  = isBrowser ?  8 :  51;
+KeyboardEvent.VK_ENTER      = isBrowser ? 13 :  36;
+KeyboardEvent.VK_SPACE      = isBrowser ? 32 :  49;
+KeyboardEvent.VK_DELETE     = isBrowser ? 46 : 117;
+KeyboardEvent.VK_TAB        = isBrowser ?  9 :  48;
+KeyboardEvent.VK_ESC        = isBrowser ? 27 :  53;
+KeyboardEvent.VK_UP         = isBrowser ? 38 : 126;
+KeyboardEvent.VK_DOWN       = isBrowser ? 40 : 125;
+KeyboardEvent.VK_LEFT       = isBrowser ? 37 : 123;
+KeyboardEvent.VK_RIGHT      = isBrowser ? 39 : 124;
 
 module.exports = KeyboardEvent;
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 module.exports = {
     Window: require('./Window'),
     ResourceLoader: require('./ResourceLoader'),
-    Screen: require('./Screen')
+    Screen: require('./Screen'),
+    MouseEvent: require('./MouseEvent'),
+    KeyboardEvent: require('./KeyboardEvent')
 };
