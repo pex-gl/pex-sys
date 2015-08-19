edceb7e9cd0386cb2c2003e803bc95d18cc685b5
Marcin Ignac <marcin.ignac@gmail.com>
2015-08-19 20:46:01 +0200

    Window passing on highdpi flag

--- Window.js
+++ Window.js
@@ -152,6 +152,7 @@ Window.create = function(obj){
             width      : window.settings.width || 1280,
             height     : window.settings.height || 720,
             fullscreen : window.settings.fullscreen || false,
+            highdpi    : window.settings.highdpi || 1
         },
         time : window._time,
         input : {
