725fe356a6d5a702186bc0ff6abea6a3ace52985
Marcin Ignac <marcin.ignac@gmail.com>
2015-10-10 23:25:00 +0100

    module initial documentation

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
+/**
+ * @module pex-sys
+ */
 module.exports = {
+    /**
+     * @see {Window}
+     */
     Window: require('./Window'),
+
+    /**
+     * @see {ResourceLoader}
+     */
     ResourceLoader: require('./ResourceLoader'),
+    /**
+     * @see {Screen}
+     */
+
     Screen: require('./Screen'),
+    /**
+     * @see {MouseEvent}
+     */
+
     MouseEvent: require('./MouseEvent'),
+    /**
+     * @see {KeyboardEvent}
+     */
     KeyboardEvent: require('./KeyboardEvent')
 };
