b9a28f2e4d041598ab6359f4e01ae8bdf83e5614
Marcin Ignac <marcin.ignac@gmail.com>
2015-10-10 23:24:15 +0100

    Window initial documentation

--- Window.js
+++ Window.js
@@ -23,6 +23,10 @@ var ListenerCallbackMethod = {
     WINDOW_RESIZE : 'onWindowResize'
 };
 
+/**
+ * Base window class
+ * @class
+ */
 function Window(){
     this._ctx = null;
     this._resources = { };
@@ -32,21 +36,35 @@ function Window(){
     this._keyboard = new Keyboard();
 }
 
-Window.prototype.init = function(){};
-Window.prototype.draw = function(){};
-
+/**
+ * Get current window (or canvas) width in px
+ * @return {Number}
+ */
 Window.prototype.getWidth = function(){
     return this._width;
 };
 
+/**
+ * Get current window (or canvas) height in px
+ * @return {Number}
+ */
 Window.prototype.getHeight = function(){
     return this._height;
 };
 
+/**
+ * Get current window (or canvas) aspect ratio
+ * @return {Number}
+ */
 Window.prototype.getAspectRatio = function(){
     return this.getWidth() / this.getHeight();
 };
 
+/**
+ * Get current window (or canvas) size
+ * @param {Array} array to put data into
+ * @return {Array} [width, height]
+ */
 Window.prototype.getSize = function(out){
     out = out || new Array(2);
     out[0] = this.getWidth();
@@ -54,6 +72,11 @@ Window.prototype.getSize = function(out){
     return out;
 };
 
+/**
+* Get current window (or canvas) bounds
+* @param {Array} array to put data into
+* @return {Array} [0, 0, width, height]
+*/
 Window.prototype.getBounds = function(out){
     out = out || new Array(4);
     out[0] = out[1] = 0;
@@ -62,28 +85,62 @@ Window.prototype.getBounds = function(out){
     return out;
 };
 
+/**
+ * Get WebGL Context instance associated with this window or canvas
+ * @return {Context}
+ */
 Window.prototype.getContext = function(){
     return this._ctx;
 };
 
+/**
+ * Get loaded resources
+ * @return {Object} @see ResourceLoader
+ */
 Window.prototype.getResources = function(){
     return this._resources;
 };
 
+/**
+ * Get Time instance associated with this window or canvas
+ * @return {Time}
+ */
 Window.prototype.getTime = function() {
     return this._time;
 };
 
+/**
+ * Get Mouse instance associated with this window or canvas
+ * @return {Mouse}
+ */
 Window.prototype.getMouse = function() {
     return this._mouse;
 };
 
+/**
+ * Get Keyboard instance associated with this window or canvas
+ * @return {Keyboard}
+ */
 Window.prototype.getKeyboard = function() {
     return this._keyboard;
 };
 
-Window.prototype.addEventListener = function(listenerObjOrType, method){
-    if(method === undefined){
+/**
+ * Helper method for registering multiple event listeners at once
+ * @param  {Object|String} listenerObjOrType
+ * @param  {Function} listenerObjOrType.onMouseDown
+ * @param  {Function} listenerObjOrType.onMouseUp
+ * @param  {Function} listenerObjOrType.onMouseDrag
+ * @param  {Function} listenerObjOrType.onMouseMove
+ * @param  {Function} listenerObjOrType.onMouseScroll
+ * @param  {Function} listenerObjOrType.onKeyDown
+ * @param  {Function} listenerObjOrType.onKeyPress
+ * @param  {Function} listenerObjOrType.onKeyUp
+ * @param  {Function} listenerObjOrType.onWindowResize
+ * @param  {Function} [calback] callback function requried type is String
+ */
+Window.prototype.addEventListener = function(listenerObjOrType, callback){
+    if(callback === undefined){
         if(listenerObjOrType === null || typeof listenerObjOrType !== 'object'){
             throw new Error('Invalid listener object.');
         }
@@ -129,7 +186,13 @@ Window.prototype.addEventListener = function(listenerObjOrType, method){
     //window listener
 };
 
-
+/**
+ * Create new window instance
+ * @param  {Object} obj
+ * @param  {Object} [obj.resources] - resources to load before init using @see ResourceLoader
+ * @param  {Object} obj.init - Function called once after the resources finised loading and WebGL context has been created.
+ * @param  {Object} obj.draw - Function called every frame
+ */
 Window.create = function(obj){
     var window = new Window();
 
