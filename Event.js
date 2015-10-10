1bdffea4a6a9143a26ca1a139f07c3c86da0fc77
Marcin Ignac <marcin.ignac@gmail.com>
2015-10-10 23:18:13 +0100

    Event initial documentation

--- Event.js
+++ Event.js
@@ -1,9 +1,9 @@
 /**
- * Base class Event.
- * @param {EventDispatcher} sender - The sender
+ * Base Event class.
  * @param {String} type - The type
  * @param {Object} [data] - The data
  * @constructor
+ * @class
  */
 function Event(type, data){
     this._sender = null;
@@ -18,28 +18,41 @@ function Event(type, data){
 }
 /**
  * Returns a copy of the event.
- * @returns {Event}
+ * @returns {Event} - copy of this event
  */
-
 Event.prototype.copy = function(){
-    return new Event(this._sender, this._type, this._data);
+    var evt = new Event(this._type, this._data);
+    evt.setSender(this._sender);
+    return evt;
 };
 
+/**
+ * Stop event from being passed to the subsequent event listenerers
+ */
 Event.prototype.stopPropagation = function(){
     this._stopPropagation = true;
 };
 
+/**
+ * @return {EventDispatcher} - dispatcher of the event
+ */
 Event.prototype.getSender = function(){
     return this._sender;
 };
 
+/**
+ * Used by the EventDispatcher that dispatches the event.
+ * @param  {EventDispatcher} sender - dispatcher of the event
+ */
 Event.prototype.setSender = function(sender) {
     this._sender = sender;
 };
 
+/**
+ * @return {String} type of the event
+ */
 Event.prototype.getType = function(){
     return this._type;
 };
-
 
 module.exports = Event;
